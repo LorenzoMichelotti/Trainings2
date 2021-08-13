a018aa7c9c0f43d910e131d74cce60589f155c2f
Alvesx3 <mateusbitc@gmail.com>
2021-08-13 13:40:11 -0300

    Create Schedule Model closed #4

---
+++ Trainings/Data/Models/Schedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Schedule :Base
+    {
+        public int Hours { get; set; }
+        public string Period { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
