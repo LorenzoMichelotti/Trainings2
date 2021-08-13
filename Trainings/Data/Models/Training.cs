a4d34ce5bbc3cd8344504c6ecd49710482b61f84
lorenzo <lomichelotti@hotmail.com>
2021-08-13 14:29:06 -0300

    Criado Training Model Atualizado

--- Trainings/Data/Models/Training.cs
+++ Trainings/Data/Models/Training.cs
@@ -8,9 +8,11 @@ namespace Data.Models
 {
     public class Training : Base
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
         public Profile profile { get; set; }
         public Content content { get; set; }
         public Schedule schedule { get; set; }
-        //public Evaluation evaluation { get; set; }
+        public Evaluation evaluation { get; set; }
     }
 }
